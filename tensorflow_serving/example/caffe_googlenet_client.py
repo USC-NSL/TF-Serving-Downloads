2ed411a308171b5ef44357f7cdb2eb2c0b889912
sugartom <sugartom.hu@gmail.com>
2018-01-18 14:50:22 -0800

    no TF modification, just updated caffe_googlenet_client.py to support batching and multi-threading

--- tensorflow_serving/example/caffe_googlenet_client.py
+++ tensorflow_serving/example/caffe_googlenet_client.py
@@ -28,6 +28,9 @@ import tensorflow as tf
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 
+import time
+import threading
+import sys
 
 tf.app.flags.DEFINE_string('server', 'localhost:9000',
                            'PredictionService host:port')
@@ -96,32 +99,155 @@ def display_results(image_paths, probs):
         confidence = round(probs[img_idx, class_indices[img_idx]] * 100, 2)
         print('{:20} {:30} {} %'.format(img_name, class_name, confidence))
 
+# def main(_):
+#   host, port = FLAGS.server.split(':')
+#   channel = implementations.insecure_channel(host, int(port))
+#   stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+#   # Send request
+#   # with open(FLAGS.image, 'rb') as f:
+#   if True:
+#     # See prediction_service.proto for gRPC request/response details.
+#     # data = f.read()
+#     request = predict_pb2.PredictRequest()
+#     request.model_spec.name = 'caffe_googlenet'
+#     request.model_spec.signature_name = 'predict_images'
+#     request.inputs['image_name'].CopyFrom(
+#         tf.contrib.util.make_tensor_proto(FLAGS.image))
+#     result = stub.Predict(request, 10.0)  # 10 secs timeout
+#     print(result.outputs["scores"].dtype)
+#     print(result.outputs["scores"].tensor_shape)
+
+#     tomList = []
+#     for float_val in result.outputs["scores"].float_val:
+#       tomList.append(float(float_val))
+#     tomArray = np.array(tomList, dtype=np.float32).reshape(1, 1000)
+#     # print(tomArray.shape)
+#     # print(tomArray.dtype)
+
+#     display_results([FLAGS.image], tomArray)
+
+# def main(_):
+#   host, port = FLAGS.server.split(':')
+#   channel = implementations.insecure_channel(host, int(port))
+#   stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+#   # Send request
+#   with open(FLAGS.image, 'rb') as f:
+#     # See prediction_service.proto for gRPC request/response details.
+#     data = f.read()
+#     request = predict_pb2.PredictRequest()
+#     request.model_spec.name = 'caffe_googlenet'
+#     request.model_spec.signature_name = 'predict_images'
+#     request.inputs['images'].CopyFrom(
+#         tf.contrib.util.make_tensor_proto(data, shape=[1]))
+#     result = stub.Predict(request, 10.0)  # 10 secs timeout
+#     # print(result)
+#     # print(result.outputs["scores"].dtype)
+#     print(result.outputs["scores"].tensor_shape)
+
+#     tomList = []
+#     for float_val in result.outputs["scores"].float_val:
+#       tomList.append(float(float_val))
+#     tomArray = np.array(tomList, dtype=np.float32).reshape(1, 1000)
+#     # print(tomArray.shape)
+#     # print(tomArray.dtype)
+
+#     display_results([FLAGS.image], tomArray)
+
+def myFuncWarmUp(stub, i):
+  request = predict_pb2.PredictRequest()
+  request.model_spec.name = 'caffe_googlenet'
+  request.model_spec.signature_name = 'predict_images'
+
+  batchSize = 10
+  durationSum = 0.0
+  runNum = 13
+
+  for k in range(runNum):
+    image_data = []
+    start = time.time()
+    for j in range(batchSize):
+      image = "/home/yitao/Downloads/inception-input/%s/dog-%s.jpg" % (str(i % 100).zfill(3), str(j).zfill(3))
+      with open(image, 'rb') as f:
+        image_data.append(f.read())
+
+    request.inputs['images'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(image_data, shape=[len(image_data)]))
+
+    result = stub.Predict(request, 60.0)  # 10 secs timeout
+    # print(result)
+    end = time.time()
+    duration = (end - start)
+    print("it takes %s sec" % str(duration))
+    if (k != 0 and k != 3 and k != 8):
+      durationSum += duration
+    # sys.stdout.write('.')
+    # sys.stdout.flush()
+
+  print("[Warm up] on average, it takes %s sec to run a batch of %d images over %d runs" % (str(durationSum / (runNum - 3)), batchSize, (runNum - 3)))
+
+def myFuncParallel(stub, i):
+  request = predict_pb2.PredictRequest()
+  request.model_spec.name = 'caffe_googlenet'
+  request.model_spec.signature_name = 'predict_images'
+
+  batchSize = 10
+  durationSum = 0.0
+  runNum = 1
+
+  for k in range(runNum):
+    image_data = []
+    start = time.time()
+    for j in range(batchSize):
+      image = "/home/yitao/Downloads/inception-input/%s/dog-%s.jpg" % (str(i % 100).zfill(3), str(j).zfill(3))
+      with open(image, 'rb') as f:
+        image_data.append(f.read())
+
+    request.inputs['images'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(image_data, shape=[len(image_data)]))
+
+    result = stub.Predict(request, 60.0)  # 10 secs timeout
+    # print(result)
+    end = time.time()
+    duration = (end - start)
+    print("[thread-%d] it takes %s sec" % (i, str(duration)))
+    # if (k != 0 and k != 3 and k != 8):
+    if True:
+      durationSum += duration
+    # sys.stdout.write('.')
+    # sys.stdout.flush()
+
+  print("[Parallel-thread-%d] on average, it takes %s sec to run a batch of %d images over %d runs" % (i, str(durationSum / runNum), batchSize, runNum))
+
 def main(_):
+  # start = time.time()
+
   host, port = FLAGS.server.split(':')
   channel = implementations.insecure_channel(host, int(port))
   stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-  # Send request
-  # with open(FLAGS.image, 'rb') as f:
-  if True:
-    # See prediction_service.proto for gRPC request/response details.
-    # data = f.read()
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'caffe_googlenet'
-    request.model_spec.signature_name = 'predict_images'
-    request.inputs['image_name'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(FLAGS.image))
-    result = stub.Predict(request, 10.0)  # 10 secs timeout
-    # print(result.outputs["scores"].dtype)
-    # print(result.outputs["scores"].tensor_shape)
 
-    tomList = []
-    for float_val in result.outputs["scores"].float_val:
-      tomList.append(float(float_val))
-    tomArray = np.array(tomList, dtype=np.float32).reshape(1, 1000)
-    # print(tomArray.shape)
-    # print(tomArray.dtype)
+  # run Inception job
+  myFuncWarmUp(stub, 0)
 
-    display_results([FLAGS.image], tomArray)
+  num_tests = 10
+  tPool = []
+  for i in range(num_tests):
+    tPool.append(threading.Thread(target = myFuncParallel, args = (stub, i)))
+
+  start = time.time()
+  for i in range(num_tests):
+    t = tPool[i]
+    t.start()
+    # time.sleep(2.0)
+
+  for i in range(num_tests):
+    t = tPool[i]
+    t.join()
+
+  end = time.time()
+
+  print('\nFinished!')
+  print('[Parallel] The total running time to run %d concurrent jobs is %s' % (num_tests, str(end - start)))
+
 
 if __name__ == '__main__':
   tf.app.run()
