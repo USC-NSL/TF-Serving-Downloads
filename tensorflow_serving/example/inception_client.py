ad02c6e9ad4740fd25eb86db7fcc46475d9535bd
sugartom <sugartom.hu@gmail.com>
2018-02-01 20:05:46 -0800

    code backup before add new args in Sess.Run()

--- tensorflow_serving/example/inception_client.py
+++ tensorflow_serving/example/inception_client.py
@@ -137,7 +137,7 @@ def myFuncParallel(stub, i):
 
   batchSize = 100
   durationSum = 0.0
-  runNum = 10
+  runNum = 1
 
   for k in range(runNum):
     image_data = []
@@ -185,7 +185,7 @@ def main(_):
   # print("...1")
   # time.sleep(1)
 
-  num_tests = 10
+  num_tests = 2
   tPool = []
   for i in range(num_tests):
     tPool.append(threading.Thread(target = myFuncParallel, args = (stub, i)))
