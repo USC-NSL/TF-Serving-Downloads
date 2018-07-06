302a908dfaffd47e8232f3bc50d1e569a39574d7
sugartom <sugartom.hu@gmail.com>
2018-07-05 20:52:42 -0700

    added dh-yolo to activity detection

--- tensorflow_serving/example/devicehive_yolo_client.py
+++ tensorflow_serving/example/devicehive_yolo_client.py
@@ -28,6 +28,8 @@ import tensorflow as tf
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 
+from tensorflow.python.framework import tensor_util
+
 import cv2
 import numpy as np
 import time
@@ -37,8 +39,19 @@ tf.app.flags.DEFINE_string('server', 'localhost:9000',
 tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
 FLAGS = tf.app.flags.FLAGS
 
+def printResult(result, labels):
+  boxes = result.outputs['boxes']
+  classes = result.outputs['classes']
+  scores = result.outputs['scores']
+
+  tt = tensor_util.MakeNdarray(classes)
+  # for t in tt:
+  #   print(labels[t])
 
 def main(_):
+
+  labels = open("/home/yitao/Documents/fun-project/devicehive-video-analysis/data/yolo2/yolo2.names").read().splitlines()
+
   host, port = FLAGS.server.split(':')
   channel = implementations.insecure_channel(host, int(port))
   stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
@@ -47,7 +60,8 @@ def main(_):
   request.model_spec.name = 'devicehive_yolo'
   request.model_spec.signature_name = 'predict_images'
 
-  image_name = "/home/yitao/Documents/TF-Serving-Downloads/dog.jpg"
+  # image_name = "/home/yitao/Documents/TF-Serving-Downloads/dog.jpg"
+  image_name = "/home/yitao/Documents/fun-project/devicehive-video-analysis/1.png"
 
   iteration_list = [15, 1, 10]
   for iteration in iteration_list:
@@ -58,12 +72,14 @@ def main(_):
       data = data / 255.
       data = np.expand_dims(data, 0)
 
+      print(data.shape)
+
       request.inputs['input'].CopyFrom(
         tf.contrib.util.make_tensor_proto(data, dtype = np.float32, shape=data.shape))
 
       result = stub.Predict(request, 10.0)
 
-      print(result)
+      printResult(result, labels)
     end = time.time()
     print("It takes %s sec to run %d images for YOLO-devicehive" % (str(end - start), iteration))
   
